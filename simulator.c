74423ac1f1e1925ed3cb4bc5f241153804bfd41c
subodh-baniya <baniyasubodh07@gmail.com>
2025-12-20 20:32:49 +0545

    added the vehicle spacing
    spacing works but the movement of vehicle became unstable

--- simulator.c
+++ simulator.c
@@ -16,6 +16,8 @@
 #define VEHICLE_SPEED 2.0f
 #define VEHICLE_SIZE 12
 #define STOPPING_DISTANCE 30.0f
+#define MIN_SPACING 20.0f
+#define MIN_FRONT_SPACING 25.0f
 #define sleep_ms(x) Sleep(x)
 #define GREEN_LIGHT 0
 #define RED_LIGHT 1
@@ -133,34 +135,81 @@ void spawn_coords_for(int road, int logicalLane, float* outx, float* outy) {
     spawn_coords_for_fixed(road, laneIndex, outx, outy);
 }
 
+static int checkTooCloseInLane(Lane* l, float x, float y, int skipIndex) {
+    for (int i = 0; i < l->count; i++) {
+        if (i == skipIndex) continue;
+        Vehicle* other = getLaneVehicle(l, i);
+        if (!other) continue;
+        float dist = distance(x, y, other->x, other->y);
+        if (dist < MIN_SPACING) return 1;
+    }
+    return 0;
+}
+
+static float getDistanceToVehicleAhead(Lane* L, int road, int vehicleIndex) {
+    Vehicle* current = getLaneVehicle(L, vehicleIndex);
+    if (!current) return 999999.0f;
+
+    float minDist = 999999.0f;
+
+    for (int i = vehicleIndex + 1; i < L->count; i++) {
+        Vehicle* ahead = getLaneVehicle(L, i);
+        if (!ahead) continue;
+
+        float distAlongRoad = 0.0f;
+        switch (road) {
+        case 0: distAlongRoad = ahead->y - current->y; break;
+        case 1: distAlongRoad = current->x - ahead->x; break;
+        case 2: distAlongRoad = current->y - ahead->y; break;
+        case 3: distAlongRoad = ahead->x - current->x; break;
+        }
+
+        if (distAlongRoad > 0 && distAlongRoad < minDist) {
+            minDist = distAlongRoad;
+        }
+    }
+
+    return minDist;
+}
+
 //Vehicle movement
 
 static void moveLaneL3(Lane* L, int road) {
     float dx = 0.0f, dy = 0.0f;
 
-    // Set movement direction based on road
     if (road == 0) { dy = -VEHICLE_SPEED; }     
-    else if (road == 1) { dx = VEHICLE_SPEED; } 
-    else if (road == 2) { dy = VEHICLE_SPEED; }  
-    else { dx = -VEHICLE_SPEED; }               
-    for (int i = 0; i < L->count; i++) {
+    else if (road == 1) { dx = VEHICLE_SPEED; }  
+    else if (road == 2) { dy = VEHICLE_SPEED; } 
+    else { dx = -VEHICLE_SPEED; }                
+    int i = 0;
+    while (i < L->count) {
         Vehicle* v = getLaneVehicle(L, i);
-        if (!v) continue;
-
-        v->x += dx;
-        v->y += dy;
-        v->isStopped = 0;
-
-        if (v->x < -100 || v->x > SCREEN_W + 100 || v->y < -100 || v->y > SCREEN_H + 100) {
-            for (int j = i; j < L->count - 1; j++) {
-                L->data[(L->front + j) % MAX_QUEUE] = L->data[(L->front + j + 1) % MAX_QUEUE];
-            }
-            L->count--;
-            i--; 
+        if (!v) {
+            i++;
+            continue;
         }
+
+        float newx = v->x + dx;
+        float newy = v->y + dy;
+
+        if (newx < -100 || newx > SCREEN_W + 100 || newy < -100 || newy > SCREEN_H + 100) {
+            Vehicle temp;
+            dequeue(L, &temp);
+            continue;
+        }
+
+        if (!checkTooCloseInLane(L, newx, newy, i)) {
+            v->x = newx;
+            v->y = newy;
+            v->isStopped = 0;
+        }
+        else {
+            v->isStopped = 1;
+        }
+
+        i++;
     }
 }
-
 
 void moveLaneTowardCenter(Lane* L, int road) {
     float mvx = 0.0f, mvy = 0.0f;
@@ -182,18 +231,41 @@ void moveLaneTowardCenter(Lane* L, int road) {
         else if (road == 2) distToIntersection = v->y - (cy + ROAD_W / 2.0f);
         else distToIntersection = cx - ROAD_W / 2.0f - v->x;
 
-
         if ((lightState == RED_LIGHT || currentGreen != road) &&
             distToIntersection < STOPPING_DISTANCE && distToIntersection > 0) {
             v->isStopped = 1;
             continue;
         }
 
-        v->x += mvx;
-        v->y += mvy;
-        v->isStopped = 0;
+        float distToAhead = getDistanceToVehicleAhead(L, road, i);
 
-   
+        if (distToAhead < STOPPING_DISTANCE) {
+            if (i + 1 < L->count) {
+                Vehicle* ahead = getLaneVehicle(L, i + 1);
+                if (ahead && ahead->isStopped) {
+                    v->isStopped = 1;
+                    continue;
+                }
+            }
+
+            if (distToAhead < MIN_FRONT_SPACING) {
+                v->isStopped = 1;
+                continue;
+            }
+        }
+
+        float newx = v->x + mvx;
+        float newy = v->y + mvy;
+
+        if (!checkTooCloseInLane(L, newx, newy, i)) {
+            v->x = newx;
+            v->y = newy;
+            v->isStopped = 0;
+        }
+        else {
+            v->isStopped = 1;
+        }
+
         if (v->x < -100 || v->x > SCREEN_W + 100 || v->y < -100 || v->y > SCREEN_H + 100) {
             for (int j = i; j < L->count - 1; j++) {
                 L->data[(L->front + j) % MAX_QUEUE] = L->data[(L->front + j + 1) % MAX_QUEUE];
